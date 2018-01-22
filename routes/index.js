1eaed9a1ed4732d1e53fc43b5e91b392cdd4972d
Nitish Mittal <nitishmittal1990@gmail.com>
2018-01-22 18:05:36 +0530

    final changes

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
-const testFolder = './videos/';
 const fs = require('fs');
+var recursive = require("recursive-readdir");
+// const testFolder = './videos/';
 
 const arr = [];
-
+const testFolder = "./videos";
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  fs.readdir(testFolder, (err, files) => {
+  recursive(testFolder, function (err, files) {
+    // `files` is an array of absolute file paths 
+    console.log(files);
     files.forEach(file => {
-      arr.push(file);
+      console.log(file.split("/").pop());
+      arr.push(file.split("/").pop());
     });
-    fs.writeFile("studentidlist.txt",arr, function(err){
-      if(err) {
-        console.log(err);
-        
-      }
-      
-      console.log("file was saved");
-      
-    });
-    console.log(arr);
-  })
+    
+    var file = fs.createWriteStream('array.txt');
+    file.on('error', function (err) { /* error handling */ });
+    arr.forEach(function (v) { file.write(v + '\n'); });
+    file.end();
+    arr.length = 0;
+  });
+  
   res.render('index', { title: 'Express' });
 
 });
